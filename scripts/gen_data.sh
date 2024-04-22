c0708757623aeb993de6da510595aa0e961c64a8
BachNgoH <nlmbao2015@gmail.com>
2024-04-23 00:04:16 +0700

    refactor(citation_train): refactor training code

--- scripts/gen_data.sh
+++ scripts/gen_data.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=2,3 python3 generate_data.py \
     --dataset_name="BachNgoH/ParsedArxivPapers_12k" \
     --load_local=True \
-    --service="vllm" \
-    --model_name="BachNgoH/Gemma_7b_Citation_16bit" \
+    --service="groq" \
+    --model_name="llama3-70b-8192" \
     --output_path="./outputs" \
