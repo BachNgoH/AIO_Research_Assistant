13a91331c11f0f18e7ef6ac6a9db45ab1ad0f557
BachNgoH <nlmbao2015@gmail.com>
2024-04-30 18:32:33 +0700

    feat (api): start api endpoints

---
+++ api/service.py
@@ -0,0 +1,10 @@
+from src.tools import load_paper_search_tool, load_ds_tool, load_code_tool, load_graph_search_tool
+
+class AssistantService:
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def predict():
+        pass
