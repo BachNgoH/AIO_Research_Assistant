259a1a167636ffd184fd46b4a656d2a069d9f043
BachNgoH <nlmbao2015@gmail.com>
2024-05-06 10:53:59 +0700

    feat: complete function calling and link reference

--- src/tools/paper_search_tool.py
+++ src/tools/paper_search_tool.py
@@ -4,7 +4,7 @@ from llama_index.core import VectorStoreIndex
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import StorageContext
-from llama_index.core.tools import QueryEngineTool
+from llama_index.core.schema import MetadataMode
 
 from llama_index.core import QueryBundle
 from llama_index.core.postprocessor.types import BaseNodePostprocessor
@@ -12,9 +12,14 @@ from llama_index.core.schema import NodeWithScore
 from typing import List, Optional
 from llama_index.core.tools import FunctionTool
 
-class LinkNodePostprocessor(BaseNodePostprocessor):
+simple_content_template = """
+Paper link: {paper_link}
+Paper: {paper_content}
+"""
+
+class NodePostprocessor:
     def _postprocess_nodes(
-        self, nodes: List[NodeWithScore], query_bundle: Optional[QueryBundle]
+        self, nodes: List[NodeWithScore]
     ) -> List[NodeWithScore]:
         # subtracts 1 from the score
 
@@ -37,10 +42,27 @@ def load_paper_search_tool():
 
     paper_retriever = paper_index.as_retriever(
         similarity_top_k=5,
-        node_postprocessors=[LinkNodePostprocessor()]
     )
+    
+    def retrieve(query_str: str):
+        retriver_response =  paper_retriever.retrieve(query_str)
+        retriever_result = []
+        for n in retriver_response:
+            paper_id = list(n.node.relationships.items())[0][1].node_id
+            paper_content = n.node.get_content(metadata_mode=MetadataMode.LLM)
+            
+            paper_link = f"https://arxiv.org/abs/{paper_id}"
+            retriever_result.append(
+                simple_content_template.format(
+                    paper_link=paper_link, 
+                    paper_content=paper_content
+                )
+            )
+        return retriever_result
+            
+        
     # paper_search_tool = QueryEngineTool.from_defaults(
     #     query_engine=paper_query_engine,
     #     description="Useful for answering questions related to scientific papers",
     # )
-    return FunctionTool.from_defaults(paper_retriever.retrieve)
+    return FunctionTool.from_defaults(retrieve, description="Useful for answering questions related to scientific papers")
