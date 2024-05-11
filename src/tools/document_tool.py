97b4c46208872af197c579532dd1939f6df7ee37
BachNgoH <nlmbao2015@gmail.com>
2024-05-11 10:21:42 +0700

    feat (web search): Enhance web search performance

--- src/tools/document_tool.py
+++ src/tools/document_tool.py
@@ -6,13 +6,18 @@ import requests
 from typing import Optional
 import dotenv
 from llama_index.core import VectorStoreIndex
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import StorageContext
 from llama_index.core.schema import MetadataMode
 from llama_index.core.tools import FunctionTool
+from llama_index.core.postprocessor import SentenceTransformerRerank
+from llama_index.core.schema import NodeWithScore, Document
+from src.constants import DOCUMENT_EMBEDDING_MODEL_NAME, DOCUMENT_EMBEDDING_SERVICE
 
-dotenv.load_dotenv()
+dotenv.load_dotenv(override=True)
 
 simple_content_template = """
 Link: {paper_link}
@@ -32,7 +37,15 @@ def search_output_parser(response):
         title = content["title"]
         link = content["link"]
         snippet = content["snippet"]
-        web_search_results.append(simple_web_search_template.format(title=title, search_link=link, search_content=snippet))
+        web_search_results.append(
+            NodeWithScore(
+                node=Document(text=simple_web_search_template.format(
+                    title=title, 
+                    search_link=link, 
+                    search_content=snippet)
+                ),
+                score=1
+            ))
     return web_search_results
 
 def web_search_function(query, location: Optional[str] = None):
@@ -52,8 +65,20 @@ def web_search_function(query, location: Optional[str] = None):
 
 def load_document_search_tool():
     device_type = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
-    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5", cache_folder="./models", device=device_type) # must be the same as the previous stage
-
+    if DOCUMENT_EMBEDDING_SERVICE == "ollama":
+        embed_model = OllamaEmbedding(model_name=DOCUMENT_EMBEDDING_MODEL_NAME)
+    elif DOCUMENT_EMBEDDING_SERVICE == "hf":
+        embed_model = HuggingFaceEmbedding(
+            model_name=DOCUMENT_EMBEDDING_MODEL_NAME, 
+            cache_folder="./models", 
+            device=device_type, 
+            embed_batch_size=64)
+    elif DOCUMENT_EMBEDDING_SERVICE == "openai":
+        embed_model = OpenAIEmbedding(
+            model=DOCUMENT_EMBEDDING_MODEL_NAME, 
+            api_key=os.environ["OPENAI_API_KEY"])
+    else:
+        raise NotImplementedError() 
     chroma_client = chromadb.PersistentClient(path="./DB/docs")
     chroma_collection = chroma_client.get_or_create_collection("gemma_assistant_aio_docs")
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)    
@@ -62,28 +87,36 @@ def load_document_search_tool():
     paper_index = VectorStoreIndex.from_vector_store(vector_store, storage_context=storage_context, embed_model=embed_model)
 
     paper_retriever = paper_index.as_retriever(
-        similarity_top_k=5,
+        similarity_top_k=8,
+    )
+    
+    rerank_postprocessor = SentenceTransformerRerank(
+        model='mixedbread-ai/mxbai-rerank-xsmall-v1',
+        top_n=5, # number of nodes after re-ranking, 
+        keep_retrieval_score=True
     )
     
     def retrieve_ai_concepts(query_str: str):
         
-        retriver_response =  paper_retriever.retrieve(query_str)
+        retriever_response =  paper_retriever.retrieve(query_str)
         web_search_results = web_search_function(query_str)
-        retriever_result = []
-        for n in retriver_response:
+        # retriever_result = []
+        for n in retriever_response:
             file_name = n.node.metadata["file_name"]
             # paper_id = list(n.node.relationships.items())[0][1].node_id
             paper_content = n.node.get_content(metadata_mode=MetadataMode.LLM)
             
             document_link = f"https://github.com/BachNgoH/AIO_Documents/tree/main/Documents/{file_name}"
-            retriever_result.append(
-                simple_content_template.format(
+            n.node.text = simple_content_template.format(
                     paper_link=document_link, 
                     paper_content=paper_content
-                )
             )
             
-        retriever_result += web_search_results
+        retriever_response += web_search_results
+        retriever_result = rerank_postprocessor.postprocess_nodes(
+            retriever_response,
+            query_str=query_str
+        )
         return retriever_result
             
         
