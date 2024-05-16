b2c2c599023033578e1ced0c3df96527c329c9c1
BachNgoH <nlmbao2015@gmail.com>
2024-05-16 22:23:17 +0700

    feat: update middle ware

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from api.controller import router
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 app = FastAPI()
@@ -11,6 +12,14 @@ async def root():
     return {
         "message": "Hello World!",
     }
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 
 app.include_router(router, prefix="/v1")
